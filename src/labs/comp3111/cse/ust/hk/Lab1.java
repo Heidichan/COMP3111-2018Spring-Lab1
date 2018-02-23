dee1d6823d4d48a2b2173d8e1ce044537951ff2d
Heidi <Heidi@heidis-macbook-pro.local>
2018-02-23 15:53:44 +0800

    Addded a congratulation message

--- src/labs/comp3111/cse/ust/hk/Lab1.java
+++ src/labs/comp3111/cse/ust/hk/Lab1.java
@@ -3,5 +3,6 @@ package labs.comp3111.cse.ust.hk;
 public class Lab1 {
 	public static void main(String[] args){
 		System.out.println("Lab 1: Introduction to Go=it and GitHub");
+		System.out.println("Congratulation! You learned the Git Basics");
 	}
 }
